3b3ebf75eec1e31e02abf0e423dfa70bcf610182
Karen S. Sánchez <112920282+KarenSSanchezP@users.noreply.github.com>
2026-02-10 20:08:29 -0600

    add: calculadora preliminar y merge (por funcion resta)

--- calculadora.py
+++ calculadora.py
@@ -1,16 +1,21 @@
 import producto
-import suma
 import resta
+import suma
+import division
 
 def calculadora(operacion, numero1, numero2):
     if operacion == '1':
         return suma.suma(numero1, numero2)
     elif operacion == '2':
-        return resta.resta(numero1, numero2)
+        return division.division(numero1, numero2)
     elif operacion == '3':
         return producto.producto(numero1, numero2)
+    elif operacion == '4':
+        return resta.resta(numero1, numero2)
+    else:
+        return "Operación no válida"
 
-operacion = input("Ingrese la operación (1-suma, 2-resta, 3-multiplicacion): ")
+operacion = input("Ingrese la operación (1-suma, 2-division, 3-multiplicacion, 4-resta): ")
 numero1 = int(input("Ingrese el primer número: "))
 numero2 = int(input("Ingrese el segundo número: "))
 resultado = calculadora(operacion, numero1, numero2)
